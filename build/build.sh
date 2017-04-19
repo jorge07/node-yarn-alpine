459788d03b04f67b805eced4f12af3eae2a948ff
Jorge Arco <jorge.arcoma@gmail.com>
2017-04-19 14:51:10 +0200

    Onbuild and builder

---
+++ build/build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+DOCKER_PATH=../
+
+VERSIONS=(4.8 6.10 7.9)
+
+DOCKERFILE_NAME=Dockerfile.onbuild
+
+for v in $"${VERSIONS[@]}"; do
+
+    echo "Destination: $DOCKER_PATH$v/$DOCKERFILE_NAME"
+
+    mkdir -p $DOCKER_PATH$v || true # Ignore if exist before
+
+    export NODE_VERSION=$v
+    envsubst <Dockerfile.tpl> "$DOCKER_PATH$v/$DOCKERFILE_NAME"
+done
